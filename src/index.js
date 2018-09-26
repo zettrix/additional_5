7bad1caefb8a1f3868c1c7addc3e3a1c356b2f7a
zettrix <fibreun@gmail.com>
2018-09-26 21:34:12 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var set = [];
+    var cloneStr = str;
+    var strLength = str.length;
+  
+    if(str.length%2 !== 0){
+        return false;
+    } else{
+        for(var i = 0; i < bracketsConfig.length; i++){
+            set[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+        }
+       for(var i = 0; i < cloneStr.length; i++){
+            for(var j = 0; j < set.length; j++){
+                str = str.split(set[j]).join('');
+                strLength = str.length;
+            }
+            if(strLength == 0) break;
+       }
+       if(strLength == 0){
+           return true;
+       }
+    }
+    return false;
 }
